23c1678e94e943605b4cfbfb6c6d826395539dfe
Bryan Helmkamp <bryan@brynary.com>
2012-05-02 14:31:44 -0400

    Run tests works from the class file if a corresponding spec file exists
    * Merge open_rspec_file.py and run_tests.py to share twin path logic

--- rspec.py
+++ rspec.py
@@ -3,6 +3,20 @@ import sublime_plugin
 import os, errno
 import re
 
+def get_twin_path(path):
+  spec_file = path.find("/spec/") >= 0
+
+  if spec_file:
+    if path.find("/lib/") > 0:
+      return path.replace("/spec/lib/","/lib/").replace("_spec.rb", ".rb")
+    else:
+      return path.replace("/spec/","/app/").replace("_spec.rb", ".rb")
+  else:
+    if path.find("/lib/") > 0:
+      return path.replace("/lib/", "/spec/lib/").replace(".rb", "_spec.rb")
+    else:
+      return path.replace("/app/", "/spec/").replace(".rb", "_spec.rb")
+
 class OpenRspecFileCommand(sublime_plugin.WindowCommand):
   def run(self, option):
     self.views = []
@@ -10,18 +24,7 @@ class OpenRspecFileCommand(sublime_plugin.WindowCommand):
     current_file_path = self.window.active_view().file_name()
 
     spec_file = current_file_path.find("/spec/") > 0
-
-    if spec_file:
-      if current_file_path.find("/lib/") > 0:
-        twin_path = current_file_path.replace("/spec/lib/","/lib/").replace("_spec.rb", ".rb")
-      else:
-        twin_path = current_file_path.replace("/spec/","/app/").replace("_spec.rb", ".rb")
-    else:
-      if current_file_path.find("/lib/") > 0:
-        twin_path = current_file_path.replace("/lib/", "/spec/lib/").replace(".rb", "_spec.rb")
-      else:
-        twin_path = current_file_path.replace("/app/", "/spec/").replace(".rb", "_spec.rb")
-
+    twin_path = get_twin_path(current_file_path)
     path_parts = twin_path.split("/")
     dirname = "/".join(path_parts[0:-1])
     basename = path_parts[-1]
@@ -55,3 +58,31 @@ class OpenRspecFileCommand(sublime_plugin.WindowCommand):
 
   def camelize(self, string):
       return re.sub(r"(?:^|_)(.)", lambda x: x.group(0)[-1].upper(), string)
+      import sublime
+      import sublime_plugin
+      import os, errno
+      import re
+
+class RunTests(sublime_plugin.TextCommand):
+  def run(self, edit, single):
+    path = self.view.file_name()
+
+    if path.find("/spec/") < 0:
+      twin_path = get_twin_path(path)
+      if os.path.exists(twin_path):
+        path = twin_path
+      else:
+        return sublime.error_message("You're not in a spec, bro.")
+
+    root_path = re.sub("\/spec\/.*", "", path)
+
+    if single:
+      line_number, column = self.view.rowcol(self.view.sel()[0].begin())
+      line_number += 1
+      path += ":" + str(line_number)
+
+    cmd = 'osascript '
+    cmd += '"/Users/bhelmkamp/Library/Application Support/Sublime Text 2/Packages/User/run_command.applescript"'
+    cmd += ' "cd ' + root_path + ' && time ruby ' + path + '"'
+    cmd += ' "Ruby Tests"'
+    os.system(cmd)
